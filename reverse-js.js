5908867f6e5aeb4bd4819ee9f8677afce8ce11cf
NotZeetaa <rodrigo2005contente@gmail.com>
2023-11-11 11:27:04 +0000

    tree: Initial
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

---
+++ reverse-js.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.home');
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', (event) => {
+            event.preventDefault(); // Prevent the default link behavior
+
+            // Apply a simple scaling effect on button click
+            button.style.transform = 'scale(1.1)';
+
+            // Delay the transition effect to see it clearly
+            setTimeout(() => {
+                button.style.transform = 'scale(1)';
+                // Redirect to the specified link after the transition
+                window.location.href = button.parentElement.getAttribute('href');
+            }, 300);
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.page');
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', (event) => {
+            event.preventDefault(); // Prevent the default link behavior
+
+            // Apply a simple scaling effect on button click
+            button.style.transform = 'scale(1.1)';
+
+            // Delay the transition effect to see it clearly
+            setTimeout(() => {
+                button.style.transform = 'scale(1)';
+                // Redirect to the specified link after the transition
+                window.location.href = button.parentElement.getAttribute('href');
+            }, 300);
+        });
+    });
+});
